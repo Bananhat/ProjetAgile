2ad17aadd437c1cef728b9765300ac387fd5b5a0
nicolas <21800428@etu.unicaen.fr>
2019-06-12 11:19:45 +0200

    Adding require header and footer

--- content/index.php
+++ content/index.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+		<title>Accueil</title>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	</head>
+
+	<header>
+    <?php require('header.php') ?>
+	</header>
+
+	<body>
+
+  </body>
+
+    <footer>
+        <?php require('footer.php') ?>
+    </footer>
+</html>
