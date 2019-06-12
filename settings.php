ce6b60127d02a25d111e6c7c3d18b277f16089c1
Sam <sam.go@hotmail.fr>
2019-06-12 09:53:50 +0200

    Init

---
+++ settings.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors','off');
+
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'projAgile');
+
+
+
+include_once('includes/class_database.php');
+include_once('includes/class_user.php');
+include_once('includes/class_users.php');
+include_once('includes/utils_user.php');
+
+
+session_start();
