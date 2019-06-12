f7e01122bf01d140caf997d6cbb1c22f19bb1144
Sam <sam.go@hotmail.fr>
2019-06-12 11:54:28 +0200

    Navbar

--- content/header.php
+++ content/header.php
@@ -12,7 +12,15 @@
 
 		<!--Let browser know website is optimized for mobile-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	
+
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+        <script type="text/javascript" src="navBar.js"></script>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
     <body>
+    <?php include('navbar.php'); ?>
 
