d1e6466d26e1b4cc0da51bddc2f3d61e35b6f545
ClementSauvaget <50738048+ClementSauvaget@users.noreply.github.com>
2019-06-12 11:50:35 +0200

    Update footer.php

--- content/footer.php
+++ content/footer.php
@@ -1,1 +1,20 @@
-<?php
+	</body>
+		<footer id ="footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5>Contact</h5>
+                <p></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5>Links</h5>
+              </div>
+            </div>
+          </div>
+          <div class="copyright">
+            <div class="container">
+            © 2014 PoissonRouge
+            </div>
+          </div>
+        </footer>
+</html>
