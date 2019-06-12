0551debfc1333e3165f6659a0a425dee2acca9bc
Sam <sam.go@hotmail.fr>
2019-06-12 15:35:45 +0200

    ezji

--- content/changerLesRoles.php
+++ content/changerLesRoles.php
@@ -1,16 +1,57 @@
 <?php
-<<<<<<< HEAD
-include('../includes/utils_page.php');
-get_header();
 
-=======
-    function getall()
-    {
-        global $db;
-        $req = "select * from user";
-        foreach ($req as $user)
-        {
-            echo '$user->get()';
+
+
+
+$req = "SELECT * from USER where role='initiateur'";
+$args = array($user->get('id'));
+
+$req_etats = "SELECT * FROM etats_commandes";
+$etats = $db->query_get_rows($req_etats);
+
+
+$db->prepare($req);
+$commandes = $db->execute_prepared_query($args);
+
+?>
+<h1 class="title has-text-dark has-text-weight-bold" style="text-align:center; margin-bottom:2%;margin-top:2%;">Visualiser les initiateurs</h1>
+
+<table class="table is-bordered is-striped is-narrow" style="margin: auto; margin-bottom : 2%;">
+    <thead>
+        <th>Noms</th>
+        <th>Role</th>
+    </thead>
+
+    <tbody>
+
+    <?php
+
+
+
+    foreach($commandes as $commande){
+        echo '<tr>';
+        echo '<td>'.$commande['nom_cafe'].'</td>';
+        echo '<td>'.$commande['nom_pays'].'</td>';
+        echo '<td>'.$commande['nom_importateur'].'</td>';
+        echo '<td>'.$commande['etat'].'</td>';
+
+        echo '<td>';
+        echo '<form method="POST" action="">';
+        echo '<input type="text" id="commande" name="commande" value="'.$commande['id'].'" hidden />';
+
+        echo '<div class="select" style="margin-right:1px; "><select name="etat" id="etat">';
+        foreach($etats as $etat){
+            echo '<option value="'.$etat['id'].'" '.($commande['etat_id'] == $etat['id'] ? "selected" : " " ).'>'.$etat['nom'].'</option>';
         }
-    }?>
->>>>>>> 19c3d2c3e9ec5db3de6869c86fb7018f82e0c512
+        echo '</select> </div>';
+
+        echo '<input class="button is-dark" type="submit" id="submit" name="submit" value="Mettre à jour" />';
+        echo '</form>';
+        echo '</td>';
+        echo '</tr>';
+    }
+
+    ?>
+
+    </tbody>
+</table>
---
+++ user.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 12 juin 2019 à 13:35
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `agile1_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `role` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `firstName`, `name`, `email`, `password`, `role`) VALUES
+(1, 'admin', 'admin', 'admin', 'adminagile1', 'admin');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
