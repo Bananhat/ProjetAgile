fadc9c98d0c7af09e483e09c28bb41ad13690aeb
Quentin61 <45429939+Quentin61@users.noreply.github.com>
2019-06-12 14:44:15 +0200

    Update changerLesRoles.php

--- content/changerLesRoles.php
+++ content/changerLesRoles.php
@@ -1,17 +1,11 @@
 
-<?php include('../includes/class_user.php');
-include('../includes/class_users.php');
-include('../includes/utils_user.php');
-include('../includes/utils_page.php');?>
-
-get_header();
-
 <?php
-    global $db;
-    $req = "select * from user where role like('%initiateur%')";
-    foreach ($req as $user)
+    function getall()
     {
-        echo $user->get();
-    }?>
-
-get_footer();
+        global $db;
+        $req = "select * from user where role like('%initiateur%')";
+        foreach ($req as $user)
+        {
+            echo '$user->get()';
+        }?>
+    }
