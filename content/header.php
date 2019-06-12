143494e1d7b2e21c8ab1e70591ce45f6e2c938c5
Sam <sam.go@hotmail.fr>
2019-06-12 11:59:38 +0200

    style.css

--- content/header.php
+++ content/header.php
@@ -20,6 +20,7 @@
         <script type="text/javascript" src="navBar.js"></script>
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="css/style.css" />
 	</head>
     <body>
     <?php include('navbar.php'); ?>
