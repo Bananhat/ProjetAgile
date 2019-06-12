1cb4f3cb60486aca5a535fd868ed49696c5be467
M4rkusK <m.kolozik@asc.de>
2019-06-12 15:41:31 +0200

    Added Point 4 Backlog

--- persistance/DbUserUpdater.php
+++ persistance/DbUserUpdater.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
 
+require_once ("./../settings.php");
+require_once ("DbConnector.php");
+
+$dbConnector = new DbConnector();
+$dbUserWriter = new DbUserUpdater($dbConnector);
+
+var_dump($dbUserWriter->updateUserName(1, "Neubert", "test"));
 
 class DbUserUpdater
 {
@@ -22,6 +29,42 @@ class DbUserUpdater
         $statement = $pdo->prepare('UPDATE USER set Role = :userrole where id = :userid');
 
         $statement->bindParam(':userrole', $newUserRole);
+        $statement->bindParam(':userid', $userid);
+        return $statement->execute();
+    }
+
+    public function updateUserName($userid, $newfirstname, $newname) : bool
+    {
+        try
+        {
+            $pdo = $this->pdo->getConnection();
+        }
+        catch(Exception $e)
+        {
+            return false;
+        }
+
+        $statement = $pdo->prepare('UPDATE USER set FirstName = :firstname, Name = :username where id = :userid');
+
+        $statement->bindParam(':firstname', $newfirstname);
+        $statement->bindParam(':userid', $userid);
+        $statement->bindParam(':username', $newname);
+        return $statement->execute();
+    }
+
+    public function deleteUserName($userid) : bool
+    {
+        try
+        {
+            $pdo = $this->pdo->getConnection();
+        }
+        catch(Exception $e)
+        {
+            return false;
+        }
+
+        $statement = $pdo->prepare('delete from USER where id = :userid');
+
         $statement->bindParam(':userid', $userid);
         return $statement->execute();
     }
