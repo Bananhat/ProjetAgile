6cf9e15dc7ccc4984ab28febcdf488586a09d4f9
yodaluc <luc.audabram1@gmail.com>
2019-06-12 11:29:54 +0200

    Update header.php

--- content/header.php
+++ content/header.php
@@ -1,5 +1,17 @@
-<?php
-	echo '<!Doctype html>';
-	echo '<meta charset="utf-8"/>';
-	echo '<title>Plongée</title>';
-?>
+<!Doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Plongée</title>
+		
+		<!--Import Google Icon Font-->
+		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		
+		<!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">  
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+	</head>
+</html>
