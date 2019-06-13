64e682e3958023a1fba8de625fbb2ad9c681e874
yodaluc <luc.audabram1@gmail.com>
2019-06-13 09:25:22 +0200

    Update GererEleve.php

--- content/GererEleve.php
+++ content/GererEleve.php
@@ -2,6 +2,18 @@
 include_once('../includes/utils_page.php');
 get_header();
 
+$user = get_logged_user();
 
+try {
+    $db = getInstanceOfDb();
+} catch (Exception $e) {
+
+    return false;
+}
+
+if($user)
+{
+    $reqUser=$db->query("SELECT * FROM USER WHERE role = 'student'");
+}
 
 get_footer();?>
