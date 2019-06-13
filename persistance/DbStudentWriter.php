265d8afb67c3193fa682e6c7a50877a5b30521f7
M4rkusK <m.kolozik@asc.de>
2019-06-13 13:05:11 +0200

    Fixed update function

--- persistance/DbStudentWriter.php
+++ persistance/DbStudentWriter.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
+/*
+require_once("../settings.php");
+require_once("DbConnector.php");
 
+$db = new DbStudentWriter(new DbConnector());
+//$db->writeNewStudent("test", "firstname", "lastname", 5);
+$db->updateStudentName("test", "test", "name");
+*/
 
 class DbStudentWriter
 {
@@ -10,24 +17,28 @@ class DbStudentWriter
         $this->dbConnector = $dbConnector;
     }
 
-    public function writeNewStudent($firstName,$name,$level) : bool
+    public function writeNewStudent($id, $firstName,$name,$level) : bool
     {
 
         try {
             $pdo = $this->dbConnector->getConnection();
 
         } catch (Exception $e) {
-
+            $this->dbConnector::outlog($e);
             return false;
         }
 
-        $statement = $pdo->prepare('INSERT INTO `student`(`firstName`, `name`,`level`) VALUES (:firstName,:name,:level)');
+        $statement = $pdo->prepare('INSERT INTO `student`(`id_student`, `firstName`, `name`,`level`) VALUES (:id, :firstName,:name,:level)');
 
         $statement->bindParam(':firstName', $firstName);
         $statement->bindParam(':name', $name);
         $statement->bindParam(':level', $level);
+        $statement->bindParam(':id', $id);
 
         $suc = $statement->execute();
+        var_dump($statement->errorInfo());
+        $this->dbConnector::outlog(preg_replace( "/\r|\n/", "", $statement->queryString )  ." Successfull: $suc");
+
         return $suc;
     }
 
@@ -40,14 +51,17 @@ class DbStudentWriter
         }
         catch(Exception $e)
         {
+            $this->dbConnector::outlog($e);
             return false;
         }
 
-        $statement = $pdo->prepare('UPDATE student set firstName = :firstname, name = :username where id = :userid');
+        $statement = $pdo->prepare('UPDATE student set firstname = :firstname, name = :username where id_student = :userid');
         $statement->bindParam(':firstname', $newfirstname);
         $statement->bindParam(':userid', $userid);
         $statement->bindParam(':username', $newname);
+        $suc =  $statement->execute();
+        $this->dbConnector::outlog(preg_replace( "/\r|\n/", "", $statement->queryString )  ." Successfull: $suc");
 
-        return $statement->execute();
+        return $suc;
     }
 }
