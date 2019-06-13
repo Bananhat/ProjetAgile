e546df3ef9cc42e3db2a100fe74a538948f64bd9
M4rkusK <m.kolozik@asc.de>
2019-06-13 11:29:01 +0200

    Added logging function outlog($e)

--- persistance/DbConnector.php
+++ persistance/DbConnector.php
@@ -1,5 +1,9 @@
 <?php declare(strict_types=1);
 
+$GLOBALS["db_logpath"] = 'php.log';
+$GLOBALS["bEchoLogs"] = false;
+$GLOBALS["bEnableLogging"] = true;
+$GLOBALS["bWriteLogs"] = true;
 
 class DbConnector
 {
@@ -14,12 +18,32 @@ class DbConnector
         } catch (\PDOException $exception) {
             throw new \Exception("No connection to database");
         }
+        $this::outlog("Connected to Database with connstring: $dsn");
         return $pdo;
     }
 
     public function execStatement($statement) : array
     {
         $statement->execute();
+        $this::outlog("ErrorInfo: " . $statement->errorInfo());
+
         return $statement->fetchAll(2); // FETCH_ASSOC
+    }
+
+    static function outlog($e)
+    {
+        $calling_func = str_pad(debug_backtrace()[1]['function'], 20, " ");
+        if ($GLOBALS["bEnableLogging"])
+        {
+            $logmsg =  date('Y-m-d H:i:s') . " [$calling_func] $e";
+            if ($GLOBALS["bEchoLogs"])
+            {
+                echo "<br>" .$logmsg;
+            }
+            if ($GLOBALS["bWriteLogs"])
+            {
+                file_put_contents($GLOBALS["db_logpath"], $logmsg . PHP_EOL, FILE_APPEND | LOCK_EX);
+            }
+        }
     }
 }
