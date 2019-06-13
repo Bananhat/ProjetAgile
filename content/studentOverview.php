f7542dadcf32de85637adb5fd933847de5cfbb6b
yodaluc <luc.audabram1@gmail.com>
2019-06-13 18:18:12 +0200

    update

--- content/studentOverview.php
+++ content/studentOverview.php
@@ -2,34 +2,36 @@
 include '../persistance/DbConnector.php';
 include '../persistance/DbSummaryReader.php';
 include_once '../settings.php';
+include_once('../includes/utils_page.php');
+get_header();
 
 $Dbreader = new DbSummaryReader(new DbConnector());
 $result = $Dbreader->readSummaryFromStudentId(3);
 
 $html = "";?>
 
-<table border="1">
+<table class="striped centered" id="tableau">
     <tr>
-        <td></td>
+        <td id="tableau"></td>
         <?php
         $studentid = $_GET['id'];
         $competence = $Dbreader->getCompetencesFromStudentId($studentid);
         foreach($competence as $comp)
         {
             $count = $Dbreader->getSkillCountFromCompetenceId($comp["id"]);
-            echo "<td colspan=".$count[0]["count(*)"].">".$comp['name'].'</td>';
+            echo "<td id='tableau' colspan=".$count[0]["count(*)"].">".$comp['name'].'</td>';
         }?>
     </tr>
 
     <tr>
-        <td></td>
+        <td id="tableau"></td>
         <?php
         foreach($competence as $comp)
         {
             $aptitude = $Dbreader->getSkillsFromCompetenceId($comp['id']);
             foreach($aptitude as $apt)
             {
-                echo '<td>'.$apt['skill'].'</td>';
+                echo '<td id="tableau">'.$apt['skill'].'</td>';
             }
         }?>
     </tr>
@@ -41,7 +43,7 @@ $html = "";?>
     {
         if($dat != $olddat) {
             echo '<tr>';
-            echo '<td>' . $dat['date'] . '</td>';
+            echo '<td id="tableau">' . $dat['date'] . '</td>';
 
 
         foreach($competence as $comp)
@@ -58,15 +60,15 @@ $html = "";?>
 
                 if($trial[0]["validated"] == 1)
                 {
-                    echo '<td>Acquis</td>';
+                    echo '<td id="tableau">Acquis</td>';
                 }
                 else if($trial[0]["validated"] == 2)
                 {
-                    echo '<td>En Cours</td>';
+                    echo '<td id="tableau">En Cours</td>';
                 }
                 else
                 {
-                    echo '<td> </td>';
+                    echo '<td id="tableau"> </td>';
                 }
             }
         }
@@ -75,4 +77,5 @@ $html = "";?>
         $olddat = $dat;
     }?>
 </table>
+<?php get_footer()?>
 
