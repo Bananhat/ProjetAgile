e9268482c304e1c2ed6aef10cac1987229eda1c4
yodaluc <luc.audabram1@gmail.com>
2019-06-13 14:21:17 +0200

    Update studentOverview.php

--- content/studentOverview.php
+++ content/studentOverview.php
@@ -6,4 +6,33 @@ include_once '../settings.php';
 $Dbreader = new DbSummaryReader(new DbConnector());
 $result = $Dbreader->readSummaryFromStudentId(1);
 
-var_dump($result);
+var_dump($result);?>
+
+<table>
+    <tr>
+        <?php
+        foreach($competence as $comp)
+        {
+            echo '<td colspan=count($aptitude);>'.$comp.'</td>';
+        }?>
+    </tr>
+
+    <tr>
+        <?php
+        foreach($aptitude as $apt)
+        {
+            echo '<td>'.$apt.'</td>';
+        }?>
+    </tr>
+
+    <?php
+    foreach($date as $dat)
+    {
+        echo '<tr>';
+        foreach($aptitude as $apt)
+        {
+            echo '<td>'.$studentTrial.'</td>';
+        }
+        echo '</tr>';
+    }
+</table>
