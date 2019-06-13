ccd94d6f8fd1a38ceef77649ab1ac16870fa66d4
Sam <sam.go@hotmail.fr>
2019-06-13 18:04:16 +0200

    ez

--- content/competences.php
+++ content/competences.php
@@ -1,58 +1,103 @@
 <?php
 include_once('../includes/utils_page.php');
-include_once('../persistance/DbStudentWriter.php');
-include('../persistance/DbConnector.php');
+include_once('../persistance/DbCompetenceWriter.php');
+include_once('../persistance/DbConnector.php');
+
 get_header();
 
 $user = get_logged_user();
+
 try {
     $db = getInstanceOfDb();
 } catch (Exception $e) {
-    echo "Pas d'instance de BDD";
     return false;
 }
-if($user) {
 
-    if (isset($_POST['submit']))
+if ($user) {
+
+    if($_GET['supp'] == 'del')
     {
 
-        $userId = $_GET['id'];
-        $prenom = $_POST['firstName'];
-        $nom = $_POST['name'];
+        $userID = $_GET['id'];
+        $skillUp = new DbCompetenceWriter(new DbConnector());
+        $suc = $skillUp->deleteCompetence($userID);
+        header('Location: competences.php');
+    }
 
+    if (isset($_POST['submit']) || isset($_GET['cid'])) {
 
-        $userUpdate = new DbStudentWriter(new DbConnector());
-        $suc = $userUpdate->updateStudentName($userId, $prenom, $nom);
-        if($suc)
-        {
-            echo 'mise a jour!';
-            header('Location: GererEleve.php');
-        }else{
-            echo 'erreur';
+        $non_remplis = array();
+
+        foreach ($_POST as $key => $value) {
+            if (empty($value))
+                $non_remplis[] = $key;
+        }
+
+        if (count($non_remplis) > 0) {
+            echo 'Veuillez remplir ces champs : <br />';
+
+            foreach ($non_remplis as $non_rempli) {
+                echo $non_rempli . '<br />';
+            }
+        } else {
+            $name = $_POST['name'];
+            $skillUp = new DbCompetenceWriter(new DbConnector());
+            $suc = $skillUp->writeNewCompetence($name, $user->get('levelFormation'));
+            if($suc){
+                echo'bon';
+            }
+            else{
+                echo 'pasbon';
+            }
+                header('Location: competences.php');
         }
     }
 
-    $req = 'SELECT * FROM student WHERE id_student=:id';
+
+    $req = 'SELECT * FROM competences WHERE niveau=(select levelFormation from user where id=:id)';
     $reqStudent = $db->prepare($req);
-    $reqStudent->execute(array('id' => $_GET['id']));
-    ?>
+    $reqStudent->execute(array('id' => $user->get('id')));
 
-    <h4 class="title has-text-dark has-text-weight-bold" style="text-align:center; margin-bottom:2%;margin-top:2%;">
-        Modifier les eleves</h4>
+} ?>
 
-    <?php
+    <h3 class="title has-text-dark has-text-weight-bold" style="text-align:center; margin-bottom:2%;margin-top:2%;">
+        Gerer competence</h3>
 
-    foreach ($reqStudent as $rowStudent)
-    {
-        echo '<form method="POST" action="fiche_eleve.php?id='.$_GET['id'] .'"> 
-        <input type="text" placeholder="' . $rowStudent['firstName'] . '" name="firstName" />
-        <input type="text" placeholder="' . $rowStudent['name'] . '" name="name" />
-        <input class="btn waves-effect waves-light" type="submit" id="submit" name="submit" value="Modifier" />
+    <div>
+        <table class="table striped" style="margin:auto; width : 50%;">
+            <thead>
+            <th>Competence</th>
+            </thead>
+
+            <tbody>
+
+            <?php
+
+            foreach ($reqStudent as $row) {
+                echo '<tr>';
+                echo '<td>' . $row['name'] . '</td>';
+
+                echo '<td><a class="waves-effect waves-light btn" href="fiche_competence.php?id='.$row['competence_id'] .'">Modifier</a></td>';
+                echo '<td><a class="waves-effect waves-light btn" href="competences.php?supp=del&id='.$row['competence_id'].'">Supprimer</a></td>';
+                echo '<td><a class="waves-effect waves-light btn" href="skills.php?competence_id='. $row['competence_id'] .'">Liste Aptitudes</a></td>';
+                echo '</tr>';
+            }
+
+            echo '<tr>
+                
+        <form method="POST" action="competences.php?cid='.$user->get('id').'">
+       <td> <label for="name"><b>Aptitude</b></label><input type="text" placeholder="name" name="name" required /></td>
+             
+            <td>      
+        <input class="btn waves-effect waves-light" type="submit" id="submit" name="submit" value="Ajouter"/>
+          </td>
         </form>
-        ';
-    }
-}
-?>
+            </td>
+            </tr>';
+            ?>
+            </tbody>
+        </table>
+    </div>
+<?php
+get_footer();
 
-
-<?php include('footer.php') ?>
---
+++ content/fiche_competence.php
@@ -0,0 +1,59 @@
+<?php
+include_once('../includes/utils_page.php');
+include_once('../persistance/DbSkillUpdater.php');
+include_once('../persistance/DbCompetenceWriter.php');
+include_once('../persistance/DbConnector.php');
+get_header();
+
+$user = get_logged_user();
+try {
+    $db = getInstanceOfDb();
+} catch (Exception $e) {
+    echo "Pas d'instance de BDD";
+    return false;
+}
+if($user) {
+
+    if (isset($_POST['submit']))
+    {
+
+        $skillId = $_GET['id'];
+        $nom = $_POST['name'];
+
+
+        $userUpdate = new DbCompetenceWriter(new DbConnector());
+        $suc = $userUpdate->updateCompetenceName($skillId, $nom);
+
+        if($suc)
+        {
+            echo 'mise a jour!';
+            header('Location: competences.php?competence_id='.$_GET['id']);
+        }else{
+            echo 'erreur';
+        }
+    }
+
+    $req = 'SELECT * FROM competences WHERE competence_id=:id';
+    $reqSkill = $db->prepare($req);
+    $reqSkill->execute(array('id' => $_GET['id']));
+    ?>
+
+    <h4 class="title has-text-dark has-text-weight-bold" style="text-align:center; margin-bottom:2%;margin-top:2%;">
+        Modifier competence</h4>
+
+    <?php
+
+    foreach ($reqSkill as $row)
+    {
+        echo '<form method="POST" action="fiche_competence.php?id='.$_GET['id'].'"> 
+ 
+        <input type="text" value="' . $row['name'] . '" name="name" />
+        <input class="btn waves-effect waves-light" type="submit" id="submit" name="submit" value="Modifier" />
+        </form>
+        ';
+    }
+}
+?>
+
+
+<?php include('footer.php') ?>
